1e3f928c00f5ef50476feae24b8e26000b566feb
graham <graham.cr.baker@gmail.com>
2020-10-13 10:55:20 -0600

    upload4

--- app/upload.py
+++ app/upload.py
@@ -12,25 +12,25 @@ def delete_file_s3(file_path):
 
 def upload_file_to_s3(file, filename, acl="public-read"):
   s3 = boto3.client(
-   "s3",
-   aws_access_key_id=current_app.config['S3_KEY_ID'],
-   aws_secret_access_key=current_app.config['S3_SECRET_KEY']
-)
+    "s3",
+    aws_access_key_id=current_app.config['S3_KEY_ID'],
+    aws_secret_access_key=current_app.config['S3_SECRET_KEY']
+  )
 
 
-    try:
-        s3.upload_fileobj(
-            file,
-            current_app.config['S3_BUCKET'],
-            file.filename,
-            ExtraArgs={
-                "ACL": acl
-                "ContentType": file.content_type
-            }
-        )
+  try:
+    s3.upload_fileobj(
+          file,
+          current_app.config['S3_BUCKET'],
+          file.filename,
+          ExtraArgs={
+              "ACL": acl
+              "ContentType": file.content_type
+          }
+    )
 
-    except Exception as e:
-        print("Something Happened: ", e)
-        return e
+  except Exception as e:
+      print("Something Happened: ", e)
+      return e
 
-    return "{}{}".format(current_app.config["S3_LOCATION"], file.filename)
+  return "{}{}".format(current_app.config["S3_LOCATION"], file.filename)
