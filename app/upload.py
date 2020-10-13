535ca455264f7c174e1c0eb52ed39f5358e6be94
graham <graham.cr.baker@gmail.com>
2020-10-13 10:50:54 -0600

    upload2

--- app/upload.py
+++ app/upload.py
@@ -1,13 +1,9 @@
-import boto3, botocore
+import boto3
 from flask import current_app
 
 
 
-s3 = boto3.client(
-   "s3",
-   aws_access_key_id=current_app.config['S3_KEY_ID'],
-   aws_secret_access_key=current_app.config['S3_SECRET_KEY']
-)
+
 
 def delete_file_s3(file_path):
   ## something
@@ -15,6 +11,13 @@ def delete_file_s3(file_path):
 
 
 def upload_file_to_s3(file, filename, acl="public-read"):
+  s3 = boto3.client(
+   "s3",
+   aws_access_key_id=current_app.config['S3_KEY_ID'],
+   aws_secret_access_key=current_app.config['S3_SECRET_KEY']
+)
+
+
     try:
         s3.upload_fileobj(
             file,
